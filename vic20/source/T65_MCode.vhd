f99c2099e49b3824ab18641fef1e3368f6499f97
wsoltys <wiso@no.way>
2015-01-04 19:18:28 +0100

    typo

--- vic20/source/T65_MCode.vhd
+++ vic20/source/T65_MCode.vhd
@@ -671,7 +671,7 @@ begin
 					Set_Addr_To <= "11"; -- BA
 				when 5 =>
 					--SaveP <= '0'; -- MIKEJ was 1
-					SaveP <= '1'; -- WSOLTYS set it back to 0 which makes digit 2 work again on output.
+					SaveP <= '1'; -- WSOLTYS set it back to 1 which makes digit 2 work again on output.
 				when others =>
 				end case;
 			else
