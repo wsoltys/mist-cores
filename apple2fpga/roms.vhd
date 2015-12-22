b94dbf1b5068cf0ac13bd4fe67d11ebb3b7a9b3b
wsoltys <wiso@no.way>
2015-12-22 15:19:08 +0100

    [AppleII] fixed: changed IRQ reset vector from FF59 to FA40

--- apple2fpga/roms.vhd
+++ apple2fpga/roms.vhd
@@ -61,7 +61,7 @@ BEGIN
 		address_aclr_a => "NONE",
 		clock_enable_input_a => "BYPASS",
 		clock_enable_output_a => "BYPASS",
-		init_file => "../roms.hex",
+		init_file => "../roms.mif",
 		intended_device_family => "Cyclone III",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
