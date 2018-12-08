e1d5e4b5864ffd27c7757cd47750ea07303f82d6
Jose Tejada <jotego@gmail.com>
2018-12-08 21:46:01 +0100

    New PSG

--- sega-master-system/vhdl/src/dac.vhd
+++ sega-master-system/vhdl/src/dac.vhd
@@ -6,20 +6,20 @@ use IEEE.NUMERIC_STD.ALL;
 entity dac is
 	Port (
 		clk	: in  STD_LOGIC;
-		input	: in  STD_LOGIC_VECTOR (5 downto 0);
+		input	: in  STD_LOGIC_VECTOR (10 downto 0);
 		output: out STD_LOGIC);
 end dac;
 
 architecture rtl of dac is
 
-	signal delta_adder: unsigned(7 downto 0);
-	signal sigma_adder: unsigned(7 downto 0);
-	signal sigma_latch: unsigned(7 downto 0) := "01000000";
-	signal delta_b		: unsigned(7 downto 0);
+	signal delta_adder: unsigned(12 downto 0);
+	signal sigma_adder: unsigned(12 downto 0);
+	signal sigma_latch: unsigned(12 downto 0) := "0000001000000";
+	signal delta_b		: unsigned(12 downto 0);
 	
 begin
 
-	delta_b <= sigma_latch(7)&sigma_latch(7)&"000000";
+	delta_b <= sigma_latch(12)&sigma_latch(12)&"00000000000";
 	delta_adder <= unsigned(input) + delta_b;
 	sigma_adder <= delta_adder + sigma_latch;
 	
@@ -27,7 +27,7 @@ begin
 	begin
 		if rising_edge(clk) then
 			sigma_latch <= sigma_adder;
-			output <= sigma_adder(7);
+			output <= sigma_adder(12);
 		end if;
 	end process;
 
